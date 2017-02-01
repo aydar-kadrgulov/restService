4fd6d7adabbfb7b6cb505d4743d612d1866660b0
Aydar Kadrgulov <Kadrgulov.AA@nsd.ru>
2017-02-01 15:23:57 +0500

    fix

---
+++ src/main/java/com/rest/AppInitializer.java
@@ -0,0 +1,23 @@
+package com.rest;
+
+import com.rest.config.AppConfig;
+import com.rest.config.WebConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[] {AppConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[] {WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/*"};
+    }
+}
